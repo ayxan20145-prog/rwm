f5b44b0e8b0db727ab48156d09afbb4c159fb5ba
ayxan <ayxan.20145@gmail.com>
2026-07-17 10:51:14 +0400

    become wm

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use x11rb::{connect, connection::Connection};
+use x11rb::{connect, connection::Connection, protocol::xproto::*};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (conn, screen_num) = connect(None)?;
     let screen = &conn.setup().roots[screen_num];
 
-    println!("Root window: {}", screen.root);
+    conn.change_window_attributes(
+        screen.root,
+        &ChangeWindowAttributesAux::new().event_mask(
+            EventMask::SUBSTRUCTURE_REDIRECT
+                | EventMask::SUBSTRUCTURE_NOTIFY
+                | EventMask::BUTTON_PRESS
+                | EventMask::POINTER_MOTION
+                | EventMask::KEY_PRESS,
+        ),
+    )?;
+
+    conn.flush()?;
 
     Ok(())
 }
