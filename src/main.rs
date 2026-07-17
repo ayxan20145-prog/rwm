c3cd9aa812df6342d9918636ccfc75179a56936a
ayxan <ayxan.20145@gmail.com>
2026-07-17 10:46:49 +0400

    connect to x and print root window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use x11rb::{connect, connection::Connection};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (conn, screen_num) = connect(None)?;
+    let screen = &conn.setup().roots[screen_num];
+
+    println!("Root window: {}", screen.root);
+
+    Ok(())
 }
